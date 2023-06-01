9db48a05e849b0fda44859e4e30739601c96f94b
StefaniaSperani <thedarkcry@hotmail.it>
2023-06-01 12:25:53 +0200

    stati e conferma password

--- add.php
+++ add.php
@@ -7,8 +7,11 @@ if (isset($_POST['submit'])) {
     $mansione = $_POST['mansione'];
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $stato = $_POST['stato'];
 
-    $sql = "INSERT INTO operators (nome, cognome, mansione, username, password) VALUES ('$nome', '$cognome', '$mansione', '$username', '$password')";
+    $statoInt = $stato === 'true' ? 'Attivo' : 'Non attivo';
+
+    $sql = "INSERT INTO operators (nome, cognome, mansione, username, password, stato) VALUES ('$nome', '$cognome', '$mansione', '$username', '$password', '$statoInt')";
 
     if ($conn->query($sql) === TRUE) {
         echo '<div class="text-center text-danger fs-3 mt-3"> Operatore aggiunto con successo! </div>';
@@ -36,7 +39,7 @@ if (isset($_POST['submit'])) {
 
     <div class="app d-flex flex-column align-items-center py-4">
         <h2 class="text-uppercase">Aggiungi operatore</h2>
-        <form id="add-form" method="post">
+        <form id="add-form" method="post" onsubmit="return validatePsw()">
             <div class="mb-3">
                 <label for="name" class="form-label">Nome</label>
                 <input type="text" class="form-control" id="name" name="nome">
@@ -61,11 +64,50 @@ if (isset($_POST['submit'])) {
                 <label for="password_confirm" class="form-label">Conferma Password</label>
                 <input type="password" class="form-control" id="password_confirm" name="confirm_password">
             </div>
+            <h5>Stato:</h5>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="stato" id="flexRadioDefault1" value="true">
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Attivo
+                </label>
+            </div>
+            <div class="form-check mb-3">
+                <input class="form-check-input" type="radio" name="stato" id="flexRadioDefault2" value="false" checked>
+                <label class="form-check-label" for="flexRadioDefault2">
+                    Non attivo
+                </label>
+            </div>
+            <!-- errore in js -->
+            <p id="error"> </p>
+
             <button type="submit" class="btn btn-primary" value="Add" name="submit">Aggiungi</button>
             <button class='btn btn-danger'><a href='operators.php'>Indietro</a></button>
         </form>
     </div>
 
+    <script>
+        let statoAttivo = document.getElementById("flexRadioDefault1");
+        let statoNonAttivo = document.getElementById("flexRadioDefault2");
+
+        statoAttivo.addEventListener("change", function () {
+            let val = statoAttivo.value;
+        });
+
+        statoNonAttivo.addEventListener("change", function () {
+            let val = statoNonAttivo.value;
+        });
+
+        function validatePsw() {
+            let password = document.getElementById("password").value;
+            let confirmPassword = document.getElementById("password_confirm").value;
+
+            if (password !== confirmPassword) {
+                error.innerHTML = "Le password non corrispondono. Riprova.";
+                return false;
+            }
+            return true;
+        }
+    </script>
 </body>
 
 </html>
